c9122238172bc8880043d1b1967705fe07d2800d
Raoul Amisial <r.amisial.25@abdn.ac.uk>
2026-04-24 10:25:38 +0100

    Creation of a data dump from sqlite, updated requirements dependencies file and updated settings file to connect to mysql server

--- GP_Radar/GP_Radar/settings.py
+++ GP_Radar/GP_Radar/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = "GP_Radar.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gp_radar',
+        'USER': 'root',
+        'PASSWORD': 'KiotAbdnAlpha1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators

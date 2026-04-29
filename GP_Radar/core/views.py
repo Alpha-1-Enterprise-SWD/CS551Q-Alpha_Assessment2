ec77058b4e47d9d02aee487fd03301fb627b28e6
Chia-Chen Wu <kevin0211722@gmail.com>
2026-04-29 23:31:54 +0100

    completed age group data returning

--- GP_Radar/core/views.py
+++ GP_Radar/core/views.py
@@ -13,30 +13,92 @@ def table(request):
     pass
 
 
-class AgeGroupData:
+class AgeGroup:
     def __init__(self):
-        self.age_group = [
-            "0~4",
-            "5~9",
-            "10~14",
-            "15~19",
-            "20~24",
-            "25~29",
-            "30~34",
-            "35~39",
-            "40~44",
-            "45~49",
-            "50~54",
-            "55~59",
-            "60~64",
-            "65~69",
-            "70~74",
-            "75~79",
-            "80~84",
-            "85+",
-        ]
-        self.females = []
-        self.males = []
+        self.range = None
+        self.female = None
+        self.male = None
+        self.total = None
+
+
+def get_age_data(practice):
+    females = GPPopulations.objects.get(practice=practice, sex="Female")
+    males = GPPopulations.objects.get(practice=practice, sex="Male")
+    age_groups = []
+    age_range = [
+        "0~4",
+        "5~9",
+        "10~14",
+        "15~19",
+        "20~24",
+        "25~29",
+        "30~34",
+        "35~39",
+        "40~44",
+        "45~49",
+        "50~54",
+        "55~59",
+        "60~64",
+        "65~69",
+        "70~74",
+        "75~79",
+        "80~84",
+        "85+",
+    ]
+    counter = 0
+    for value in [
+        females.ages00to04,
+        females.ages05to09,
+        females.ages10to14,
+        females.ages15to19,
+        females.ages20to24,
+        females.ages25to29,
+        females.ages30to34,
+        females.ages35to39,
+        females.ages40to44,
+        females.ages45to49,
+        females.ages50to54,
+        females.ages55to59,
+        females.ages60to64,
+        females.ages65to69,
+        females.ages70to74,
+        females.ages75to79,
+        females.ages80to84,
+        females.ages85plus,
+    ]:
+        ag = AgeGroup()
+        ag.range = age_range[counter]
+        ag.female = value
+        counter += 1
+        age_groups.append(ag)
+    counter = 0
+    for value in [
+        males.ages00to04,
+        males.ages05to09,
+        males.ages10to14,
+        males.ages15to19,
+        males.ages20to24,
+        males.ages25to29,
+        males.ages30to34,
+        males.ages35to39,
+        males.ages40to44,
+        males.ages45to49,
+        males.ages50to54,
+        males.ages55to59,
+        males.ages60to64,
+        males.ages65to69,
+        males.ages70to74,
+        males.ages75to79,
+        males.ages80to84,
+        males.ages85plus,
+    ]:
+        age_groups[counter].male = value
+        age_groups[counter].total = (
+            age_groups[counter].male + age_groups[counter].female
+        )
+        counter += 1
+
+    return age_groups
 
 
 def get_Practice(request, id):
@@ -53,7 +115,7 @@ def get_Practice(request, id):
         "doctor_num": donctors.count(),
         "pat_doc_ratio": round(practice.list_size / donctors.count(), 1),
         "doctors": donctors,
-        "age_group_data": age_group_data,
+        "age_data": get_age_data(practice),
     }
 
     return render(request, "core/practice_detail.html", context)

ce14fc9aca1cbcfa2729c471af0d7e6ecfc71a21
Chia-Chen Wu <kevin0211722@gmail.com>
2026-04-29 21:48:03 +0100

    created contextual data that the frontend needs

--- GP_Radar/core/views.py
+++ GP_Radar/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from catalog.models import GPPractices
+from django.http import HttpResponse, Http404
+from catalog.models import GPPractices, GPDetails, GPPopulations
 
 # Create your views here.
 
@@ -13,6 +13,47 @@ def table(request):
     pass
 
 
+class AgeGroupData:
+    def __init__(self):
+        self.age_group = [
+            "0~4",
+            "5~9",
+            "10~14",
+            "15~19",
+            "20~24",
+            "25~29",
+            "30~34",
+            "35~39",
+            "40~44",
+            "45~49",
+            "50~54",
+            "55~59",
+            "60~64",
+            "65~69",
+            "70~74",
+            "75~79",
+            "80~84",
+            "85+",
+        ]
+        self.females = []
+        self.males = []
+
+
 def get_Practice(request, id):
-    print(id)
-    return render(request, "core/practice_detail.html")
+    try:
+        practice = GPPractices.objects.get(practice_code=id)
+    except GPPractices.DoesNotExist:
+        raise Http404("Poll does not exist")
+
+    donctors = GPDetails.objects.filter(practice=practice)
+    patients = practice.list_size
+    age_group_data = GPPopulations
+    context = {
+        "practice": practice,
+        "doctor_num": donctors.count(),
+        "pat_doc_ratio": round(practice.list_size / donctors.count(), 1),
+        "doctors": donctors,
+        "age_group_data": age_group_data,
+    }
+
+    return render(request, "core/practice_detail.html", context)
